6080d212319ee2c0cc1eb666f030e5441a3a1aaf
Training <training@mdlspc145.extra.cea.fr>
2017-05-12 11:54:47 +0200

    pippo

--- carrot_juice.f90
+++ carrot_juice.f90
@@ -12,7 +12,7 @@ PROGRAM carrot_juice
   call plant(field, 1000, 1)
   call grow(field, 1000, 3)
   call harvest(field, carrots, 1000)
-  call press(carrots, 1000, 3.145, juice)
+  call press(carrots, 1000, 3.14, juice)
 
   print*, juice, " carrot juices successfully created"
 END PROGRAM !carrot_juice
